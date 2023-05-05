26fb080cb622b5b3defd1870f6be6a8f8584c5de
dadonov <dadonov.igor@gmail.com>
2023-05-05 20:14:50 +0400

    feat: add solution to write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const { stdout, stdin, exit } = process;
+const fs = require('fs');
+const path = require('path');
+
+// creating new text.txt file
+const filePath = path.join(__dirname, 'text.txt');
+fs.writeFile(
+  filePath,
+  '',
+  (err) => {
+    if (err) throw new Error('Something got wrong');
+  }
+);
+
+stdout.write('text.txt has been created\n')
+stdout.write('Edit mode on. Press Ctrl-C or enter \'exit\' to exit\n');
+
+stdin.on('data', (data) => {
+  const stringifiedData = data.toString();
+  if (stringifiedData === 'exit\n') {
+    stdout.write('Edit mode off\n');
+    exit();
+  }
+
+  fs.appendFile(filePath,
+    stringifiedData,
+    () => {
+      stdout.write('text.txt has been edited. Continue editing or press Ctrl-C or enter \'exit\' to exit\n');
+    });
+});
+
+process.on('SIGINT', () => {
+  stdout.write('\nEdit mode off\n');
+  exit();
+})
+
