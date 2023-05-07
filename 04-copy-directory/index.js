a42efaae7db9d2f814fcb3c11f6f60a99aafc2ea
dadonov <dadonov.igor@gmail.com>
2023-05-07 18:29:30 +0400

    feat: add solution to copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
 
+const fs = require('fs');
+const path = require('path');
+const { mkdir, copyFile, readdir } = require('node:fs/promises');
+const { stdout } = require('process');
+
+const src = path.join(__dirname, 'files')
+const dest = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  const copy = await mkdir(dest, { recursive: false });
+  const files = await readdir(src);
+
+  files.forEach((file) => {
+    copyFile(path.join(src, file), path.join(dest, file));
+  })
+}
+
+//if a copy exists, delete it and create again. otherwise simply create a copy
+fs.access(dest, (error) => {
+  if (error) {
+    copyDir()
+    stdout.write('Copy of folder has been created');
+  } else {
+    fs.rm(dest, { recursive: true }, () => {
+      copyDir()
+      stdout.write('Copy of folder has been actualized');
+    })
+  }
+});
